e32398a235d1f8976e3e8064f706370ba4be397f
Joe <jmc529@vt.edu>
2019-02-26 23:34:39 -0500

    restructuring code

--- background/oauth.js
+++ background/oauth.js
@@ -1,24 +1,20 @@
-import {client_id, client_secret} from './secret.js'
-const redirect_url = browser.identity.getRedirectURL();
-const stored_state = generateRandomString(16);
+import {CLIENT_ID, CLIENT_SECRET} from './secret.js'
+
+
+/** VARIABLES **/
+const REDIRECT_URL = browser.identity.getRedirectURL();
+const STORED_STATE = generateRandomString(16);
 const scope = "user-modify-playback-state user-read-recently-played user-read-currently-playing"
 	+ " streaming user-read-birthdate user-read-email user-read-private user-library-read user-library-modify";
 
+
+/** PRIVATE FUNCTIONS **/
+/** HELPER FUNCTIONS **/
 /**	
  * Generates a random string containing numbers and letters
  * @param  {number} length The length of the string
  * @return {string} The generated string
  */
-function generateRandomString(length) {
-	let text = '';
-	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-	for (var i = 0; i < length; i++) {
-		text += possible.charAt(Math.floor(Math.random() * possible.length));
-	}
-	return text;
-}
-
 function checkResponse(response) {
 	return new Promise((resolve, reject) => {
 		if (response.status != 200) {
@@ -36,8 +32,8 @@ function checkResponse(response) {
 
 /**
  * Extract information on the passed URI, for spotify OAuth2 response
- * @param {string} uri the string to parse through
- * @return {Array} in this order: code info, state info, error info
+ * @param {string} uri The string to parse through
+ * @return {Array} In this order: code info, state info, error info
  */
 function extractOAuthInfo(uri) {
   let m = uri.match(/[#?](.*)/);
@@ -48,17 +44,34 @@ function extractOAuthInfo(uri) {
 }
 
 
+/**	
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+function generateRandomString(length) {
+	let text = '';
+	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+	for (var i = 0; i < length; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
+}
+
+
+/** MAIN FUNCTIONS **/
 /**
  * Sends a request to spotify for authorization.
- * @return {string} if promise is fullfilled returns URI with auth info
+ * @return {string} If promise is fullfilled returns URI with auth info
  */
 function authorize() {
 	let auth_url = `https://accounts.spotify.com/authorize?` +
 		`&response_type=code` +
-		`&client_id=${client_id}` +
-		`&redirect_uri=${redirect_url}` +
+		`&client_id=${CLIENT_ID}` +
+		`&redirect_uri=${REDIRECT_URL}` +
 		`&scope=${scope}` +
-		`&state=${stored_state}`;
+		`&state=${STORED_STATE}`;
 
 	return browser.identity.launchWebAuthFlow({
 		interactive: true,
@@ -67,23 +80,43 @@ function authorize() {
 }
 
 /**
+ * Sends a request to the spotify API for a new access token.
+ * @param {string} refreshToken Should be the refresh token from the validate function
+ * @return {Promise} If fullfilled holds the access token and (maybe?) refresh token
+ */
+function refreshAccessToken(refreshToken) {
+  	const validation_request = new Request("https://accounts.spotify.com/api/token", {
+		method: "POST",
+		body: `grant_type=refresh_token&refresh_token=${refreshToken}`,
+		headers: {
+			'Authorization': 'Basic ' + window.btoa(CLIENT_ID + ':' + CLIENT_SECRET),
+            'Content-Type':'application/x-www-form-urlencoded'
+		}
+	});
+	return window.fetch(validation_request).then(checkResponse)
+	.catch((err) => {
+        console.log(`Error: ${err}`);
+    });
+}
+
+/**
 * Takes uri from authorize and, if error isn't present and state hasn't been compromised, sends
-* a validation request to spotify. If that works spotify sends back data from which we grab 
-* the access and refresh token.
-* @returns {promise} if fullfilled holds the access token and refresh token
+* a validation request to spotify. 
+* @param {string} authInfo The URL that holds the user's authentication information
+* @returns {Promise} If fullfilled holds the access token and refresh token
 */
-function validate(auth_info) {
-	const [code, state, error] = extractOAuthInfo(auth_info);
+function validate(authInfo) {
+	const [code, state, error] = extractOAuthInfo(authInfo);
 	if (error || code === null) {
 		throw `Authorization failure\n${error}`;
-	} else if (state === null || state !== stored_state) {
+	} else if (state === null || state !== STORED_STATE) {
 		throw "State mismatch failure";
 	} else {
 		const validation_request = new Request("https://accounts.spotify.com/api/token", {
 			method: "POST",
-			body: `grant_type=authorization_code&code=${code}&redirect_uri=${redirect_url}`,
+			body: `grant_type=authorization_code&code=${code}&redirect_uri=${REDIRECT_URL}`,
 			headers: {
-				'Authorization': 'Basic ' + window.btoa(client_id + ':' + client_secret),
+				'Authorization': 'Basic ' + window.btoa(CLIENT_ID + ':' + CLIENT_SECRET),
                 'Content-Type':'application/x-www-form-urlencoded'
 			}
 		});
@@ -96,23 +129,10 @@ function validate(auth_info) {
 }
 
 
-function refreshAccessToken(refreshToken) {
-  	const validation_request = new Request("https://accounts.spotify.com/api/token", {
-		method: "POST",
-		body: `grant_type=refresh_token&refresh_token=${refreshToken}`,
-		headers: {
-			'Authorization': 'Basic ' + window.btoa(client_id + ':' + client_secret),
-            'Content-Type':'application/x-www-form-urlencoded'
-		}
-	});
-	return window.fetch(validation_request).then(checkResponse)
-	.catch((err) => {
-        console.log(`Error: ${err}`);
-    });
-}
-
+/** PUBLIC FUNCTIONS **/
 /**
  * Updates browser.storage's spotify token data.
+ * @return {Array} Populated with the access and refresh token in that order
  */
 async function getTokenData() {
 	const data = await browser.storage.local.get();
